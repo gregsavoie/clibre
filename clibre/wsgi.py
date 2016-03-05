f2d2e3faaa618491d333965beaecec7e8bbe4f6a
Greg Savoie <savg2688@hotmail.com>
2016-03-05 14:28:33 -0500

    correction d'un faute d'orthographe et changement de settings pour virtualenv

--- clibre/wsgi.py
+++ clibre/wsgi.py
@@ -13,8 +13,8 @@ from django.core.wsgi import get_wsgi_application
 from django.conf import settings
 
 site.addsitedir(settings.BASE_DIR + "/venv/lib/python3.4/site-packages")
-sys.path.append("/home/greg/clibre")
-sys.path.append("/home/greg/clibre/clibre")
+sys.path.append(settings.BASE_DIR)
+sys.path.append(settings.BASE_DIR + "/clibre")
 os.environ["DJANGO_SETTINGS_MODULE"] = "clibre.settings"
 activate_env=settings.BASE_DIR + "/venv/bin/activate_this.py"
 exec(open(activate_env, 'r').read())
